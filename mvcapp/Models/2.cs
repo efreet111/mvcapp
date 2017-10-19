257cd021a2b7f59bde9e81b5a7cb5602d961917d
Victor A. Silgado <efreet111@gmail.com>
2017-10-19 15:15:33 -0430

    Prueba 1

---
+++ mvcapp/Models/2.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace mvcapp.Models
+{
+    public class _2
+    {
+    }
+}
---
+++ mvcapp/Models/UsuarioModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace mvcapp.Models
+{
+    public class UsuarioModel
+    {
+        public class ActorModel
+        {
+            [Required]
+            [Display(Name = "Nombre")]
+            public string Name { get; set; }
+            [Required]
+            [Display(Name = "Apellido")]
+            public string LastName { get; set; }
+            [Key]
+            [Required]
+            [Display(Name = "ID")]
+            public string IDNumber { get; set; }
+            [Key]
+            [Required]
+            [DataType(DataType.EmailAddress)]
+            [Display(Name = "EMail")]
+            public string EMail { get; set; }
+            
+            [Required]
+            [DataType(DataType.Password)]
+            [Display(Name = "password")]
+            public string Password { get; set; }
+        }
+    }
+
+
+    
+}
